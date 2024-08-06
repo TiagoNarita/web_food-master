e64cce34285ca8b664407754b45b2305122cfeb2
Tiago <tiagobnarita@gmail.com>
2024-08-05 23:02:17 -0300

    bom dmss

--- src/components/HeaderPedidos/index.jsx
+++ src/components/HeaderPedidos/index.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
+import Modal from "react-modal";
 import "./styles.css";
 
+Modal.setAppElement("#root"); // Define o elemento base da aplicação para acessibilidade
+
 export const HeaderPedidos = () => {
-  const handleLogin = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
     console.log("oi");
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   return (
@@ -20,7 +30,7 @@ export const HeaderPedidos = () => {
             <span>Início</span>
           </button>
         </Link>
-        <Link to="/promocoes" className="navLink">
+        <Link to="/pedidos" className="navLink">
           <button className="navButton">
             <ShoppingCartIcon />
             <span>Pedidos</span>
@@ -32,13 +42,22 @@ export const HeaderPedidos = () => {
             <span>Fale conosco</span>
           </button>
         </Link>
-        <Link to="/entrar" className="navLink">
-          <button className="navButton" onClick={handleLogin}>
-            <PersonIcon />
-            <span>Entrar/Cadastrar</span>
-          </button>
-        </Link>
+        <button className="navButton" onClick={openModal}>
+          <PersonIcon />
+          <span>Entrar/Cadastrar</span>
+        </button>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Login Modal"
+        className="modalContent"
+        overlayClassName="modalOverlay"
+      >
+        <div className="closeModal"></div>
+        <div></div>
+      </Modal>
     </div>
   );
 };
